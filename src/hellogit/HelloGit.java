05d7f7a9fbce1fd2aa253ffa409e26151d26b9c9
Jean-Marc Chapuzot <chapsjmg@icloud.com>
2022-04-04 11:52:18 +0200

    New line

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -27,6 +27,7 @@ public class HelloGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World .... with git!");
+        System.out.println(" New line to add....");
     }
     
 }
